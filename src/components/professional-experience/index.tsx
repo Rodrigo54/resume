cb12bfb78de84317c8a4a9ba2a20281db5e8e972
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2025-06-16 10:54:00 -0300

    refactor: replace sort with toSorted and improve key usage in ProfessionalExperience component

--- src/components/professional-experience/index.tsx
+++ src/components/professional-experience/index.tsx
@@ -4,34 +4,26 @@ import { CalendarDays as Calendar } from '@styled-icons/fa-regular/CalendarDays'
 import { HomeWork } from "@styled-icons/material-rounded/HomeWork";
 import { Work } from "@styled-icons/material/Work";
 import { jobs } from "database.json";
-import { capitalize } from "lodash";
 import Title from "@components/title";
 import Blockquote from "@components/blockquote";
-import extenso from 'extenso';
-import { format, parse } from 'date-fns';
+import { formatDistance, parse } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import DateFormat from '@components/date-format';
 
 const ProfessionalExperience: React.FC = () => {
-  function dateFormat(date?: string) {
-    if (date) {
-      const dateObj = parse(date, 'yyyy-MM-dd', new Date());
-      const stringDate = format(dateObj, `MMMM 'de' yyyy`, { locale: ptBR });
-      return <time dateTime={date}>{capitalize(stringDate)}</time>;
-    } else {
-      return <span>Até o momento</span>;
-    }
-  }
-
   return (
     <S.ContentWrapper>
       <Title>Experiências Profissionais</Title>
       {jobs
-        .sort((a, b) => b.duration.init.localeCompare(a.duration.init))
+        .toSorted((a, b) => b.duration.init.localeCompare(a.duration.init))
         .slice(0, 3)
-        .map((item, index) => {
+        .map((item) => {
+          const initDate = parse(item.duration.init, 'yyyy-MM-dd', new Date());
+          const endDate = item.duration.end ? parse(item.duration.end, 'yyyy-MM-dd', new Date()) : new Date();
+          const duration = formatDistance(initDate, endDate, { locale: ptBR });
+
           return (
-            <S.WorkWrap key={index}>
+            <S.WorkWrap key={`${item.locale}-${item.position}`}>
               <S.InfoWrap>
                 <S.InfoName>
                   <HomeWork size={12} /> {item.locale}
@@ -41,15 +33,18 @@ const ProfessionalExperience: React.FC = () => {
                 </S.InfoLocale>
                 <S.InfoDate>
                   <Calendar size={12} />
-                  <DateFormat date={item.duration.init} /> -
+                  <DateFormat date={item.duration.init} />
+                  {' - '}
                   <DateFormat date={item.duration.end} />
+                  {' • '}
+                  <span>{duration}</span>
                 </S.InfoDate>
               </S.InfoWrap>
               <Blockquote>{item.description}</Blockquote>
             </S.WorkWrap>
           );
         })}
-      <S.InfoFooter>E mais outras {extenso(jobs.length - 3)} experiências profissionais.</S.InfoFooter>
+      <S.InfoFooter>E mais outras {jobs.length - 3} experiências profissionais.</S.InfoFooter>
       <S.InfoFooter>Veja no Linkedin todas as experiências profissionais.</S.InfoFooter>
     </S.ContentWrapper>
   );
