03f6be761112e8981fa93f4b6b2dd89da9dfba7f
Rodrigo Alves <rodrigo54mix@gmail.com>
2026-03-30 22:12:53 -0300

    feat: migrate to Tailwind CSS and remove styled-components styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ReactApp from './app';
-import GlobalStyles from '@styles/global';
-import PrintStyles from '@styles/print';
-import ThemeStyles from '@styles/theme';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <PrintStyles />
-    <ThemeStyles />
     <ReactApp />
   </React.StrictMode>
 )
