250cf505f36e3dfeeb716077cba35227cd9dc6bc
Rodrigo Alves <rodrigo54mix@gmail.com>
2026-03-30 22:08:37 -0300

    feat: add Links component and remove styled-components styles

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,22 +1,22 @@
-import * as S from "./styles";
 import { links } from "database.json";
 import Title from "@components/title";
 
 const Links: React.FC = () => {
   return (
-    <S.ContentWrapper>
+    <section className="flex flex-col items-start gap-[5px]">
       <Title>Links</Title>
       {links.map((link) => (
-        <S.Link
+        <a
           key={link}
           href={`${link}`}
           target="_blank"
           rel="noopener noreferrer"
+          className="[font-family:var(--font-body)] text-base font-medium leading-[1.2] break-keep text-[var(--text-color)] no-underline hover:text-[var(--color1-light)]"
         >
           <p>{link}</p>
-        </S.Link>
+        </a>
       ))}
-    </S.ContentWrapper>
+    </section>
   );
 };
 
