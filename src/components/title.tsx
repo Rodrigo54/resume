818400746de83a530f2f0be9f9d6779bd1912eaf
Rodrigo Alves <rodrigo54mix@gmail.com>
2026-03-30 23:00:49 -0300

    fix: adjust margin spacing in Title component

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Title: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <h2 className="[font-family:var(--font-title)] text-[var(--color1)] break-words tracking-[0.01rem] leading-none font-extrabold text-[1.3rem] my-[7px]">
+  <h2 className="[font-family:var(--font-title)] text-[var(--color1)] break-words tracking-[0.01rem] leading-none font-extrabold text-[1.3rem] my-[6px]">
     {children}
   </h2>
 );
