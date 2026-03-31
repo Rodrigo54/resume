7eeafa9f10289bf9cf30da08d356a32ae990129a
Rodrigo Alves <rodrigo54mix@gmail.com>
2026-03-30 22:12:37 -0300

    feat: create Title component and remove styled-components implementation

---
+++ src/components/title.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Title: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <h2 className="[font-family:var(--font-title)] text-[var(--color1)] break-words tracking-[0.01rem] leading-none font-extrabold text-[1.3rem] my-[7px]">
+    {children}
+  </h2>
+);
+
+export default Title;
