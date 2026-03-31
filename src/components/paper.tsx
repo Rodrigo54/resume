ef83502741f8073979bb7eb36c7461281fe282a2
Rodrigo Alves <rodrigo54mix@gmail.com>
2026-03-30 22:08:53 -0300

    feat: implement Paper component and remove styled-components styles

--- src/components/paper.tsx
+++ src/components/paper.tsx
@@ -12,10 +12,6 @@ import { DarkMode } from '@styled-icons/material-outlined/DarkMode';
 import { LightMode } from '@styled-icons/material-outlined/LightMode';
 import { useThemeCss } from '@utils/theme';
 
-import * as S from './styles';
-
-
-
 const Paper: React.FC = () => {
 
   const [theme, setTheme] = useThemeCss('dark');
@@ -34,36 +30,38 @@ const Paper: React.FC = () => {
     setTheme(isDarkMode ? 'light' : 'dark');
   };
 
+  const actionBtn = "w-[40px] h-[40px] flex flex-row flex-nowrap items-center justify-center bg-transparent border-none rounded-full text-[var(--color1)] cursor-pointer p-0 focus:outline-2 focus:outline-transparent focus:outline-offset-2 hover:bg-[var(--color1-shade)] hover:text-[var(--color1-contrast)]";
+
   return (
-    <S.PaperWrapper>
-      <S.ActionsContainer>
-        <S.ActionButton onClick={() => print()}>
+    <div className="flex flex-row flex-nowrap items-start justify-center min-h-screen bg-[image:var(--background-gradient)] py-[5rem] px-0">
+      <div className="py-[50px] px-0 w-[70px] flex flex-col items-center justify-center gap-[10px]">
+        <button className={actionBtn} onClick={() => print()}>
           <Print size={36} />
-        </S.ActionButton>
-        <S.ActionButton onClick={() => link()}>
+        </button>
+        <button className={actionBtn} onClick={() => link()}>
           <Github size={30} />
-        </S.ActionButton>
-        <S.ActionButton onClick={() => setPreferredTheme()}>
+        </button>
+        <button className={actionBtn} onClick={() => setPreferredTheme()}>
           {isDarkMode ? <DarkMode size={30}/> : <LightMode size={30}/>}
-        </S.ActionButton>
-      </S.ActionsContainer>
-      <S.PaperContainer>
-        <S.PaperArticle>
+        </button>
+      </div>
+      <main className="p-[40px] m-0 w-[85vw] max-w-[900px] bg-[var(--paper-background)] shadow-[var(--paper-shadow)] grid grid-cols-[1fr_0.5fr] gap-[30px]">
+        <article className="flex flex-col gap-[5px]">
           <HeaderResume />
           <ProfessionalExperience />
           <AcademicFormation />
-        </S.PaperArticle>
-        <S.PaperAside>
+        </article>
+        <aside className="flex flex-col gap-[10px]">
           <Contact />
           <Skills />
           <Courses />
           <Languages />
           <Links />
-        </S.PaperAside>
-      </S.PaperContainer>
-      <S.ActionsContainer />
-    </S.PaperWrapper>
-  )
+        </aside>
+      </main>
+      <div className="py-[50px] px-0 w-[70px]" />
+    </div>
+  );
 }
 
 export default Paper;
